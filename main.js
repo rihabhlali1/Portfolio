2357d9060749300d4590ccf5efe1c474384d1638
rihabhlali1 <158101353+rihabhlali1@users.noreply.github.com>
2024-05-07 00:27:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Telecommunications engineering Student." , "Graphic Designer."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+   
